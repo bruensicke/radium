a4efd698a5990cded64db90de83225c860ae8f38
d1rk <d1rk@gmx.de>
2012-07-23 16:03:48 +0200

    adding BaseModel and Configurations

---
+++ models/BaseModel.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * Base class for all Models
+ *
+ * If you have models in your app, you should extend this class like that:
+ *
+ * {{{
+ *  class MyModel extends \radium\model\BaseModel {
+ * }}}
+ *
+ * @see app\models
+ * @see lithium\data\Model
+ */
+namespace radium\models;
+
+use radium\models\Configurations;
+
+use lithium\core\Libraries;
+use lithium\util\Inflector;
+
+class BaseModel extends \lithium\data\Model {
+
+	/**
+	 * Custom status options
+	 *
+	 * @var array
+	 */
+	public static $_status = array(
+		'active' => 'active',
+		'inactive' => 'inactive',
+	);
+
+	/**
+	 * Custom type options
+	 *
+	 * @var array
+	 */
+	public static $_types = array();
+
+	/**
+	 * Custom find query properties, indexed by name.
+	 *
+	 * @var array
+	 */
+	public static $__finders = array(
+		'deleted' => array(
+			'conditions' => array(
+				'deleted' => 'IS NOT NULL',
+			)
+		)
+	);
+
+	/**
+	 * Required by lithium
+	 *
+	 * @see lithium\data\Model
+	 * @return void
+	 */
+	public static function __init() {
+		static::_isBase(__CLASS__, true);
+		parent::__init();
+
+		$finders = array_merge(static::$_status, static::$_types, static::$__finders);
+		foreach (static::$_status as $key => $value) {
+			if (is_numeric($key)) $key = $name;
+			static::finder($key, array('conditions' => array('status' => $key)));
+		}
+
+		foreach (static::$_types as $key => $value) {
+			if (is_numeric($key)) $key = $name;
+			static::finder($key, array('conditions' => array('type' => $key)));
+		}
+
+		// auto-update the created and updated fields
+		static::applyFilter('save', function ($self, $params, $chain) {
+			$field = ($params['entity']->exists()) ? 'updated' : 'created';
+			$params['entity']->$field = date(DATE_ATOM);
+			return $chain->next($self, $params, $chain);
+		});
+
+		// soft-delete on all rows, that have a 'deleted' field in schema
+		static::applyFilter('delete', function ($self, $params, $chain) {
+			$deleted = $params['entity']->schema('deleted');
+			if(is_null($deleted)) {
+				return $chain->next($self, $params, $chain);
+			}
+			$params['entity']->deleted = date(DATE_ATOM);
+			return $params['entity']->save();
+		});
+	}
+
+	/**
+	 * Allows to pass in a query to do, what a man needs to do.
+	 * Make sure, you are not trying to be james bond, without
+	 * beeing sure, you know what you are doing.
+	 *
+	 * Returns a lithium\data\source\database\adapter\my_sql\Result object
+	 *
+	 * @param string $sql
+	 * @return object
+	 */
+	public static function query($sql) {
+		return static::connection()->invokeMethod('_execute', array($sql));
+	}
+
+	/**
+	 * Returns all schema-fields, without their types
+	 *
+	 * @return array
+	 */
+	public static function fields() {
+		return array_keys(static::schema());
+	}
+
+	/**
+	 * fetches the associated config record
+	 *
+	 * @param object $entity current instance
+	 * @return array client data
+	 */
+	public function configuration($entity) {
+		return $entity->config = Configurations::first($entity->config_id);
+	}
+
+	/**
+	 * fetches the associated record
+	 *
+	 * @param object $entity current instance
+	 * @param string|array $name name of model to load
+	 * @return array remote object data
+	 */
+	public function resolve($entity, $fields = null) {
+		$get_class = function($name) {
+			$modelname = Inflector::pluralize(Inflector::classify($name));
+			return Libraries::locate('models', $modelname);
+		};
+
+		switch (true) {
+			case is_string($fields):
+				$fields = array("{$fields}_id");
+				break;
+			case empty($fields):
+				$fields = self::fields();
+				break;
+			case is_array($fields):
+				$fields = array_map(function($field){
+					return (stristr($field, '_id')) ? $field : "{$field}_id";
+				}, $fields);
+				break;
+		}
+
+		$return = true;
+		foreach ($fields as $field) {
+			if (preg_match('/^(.+)_id$/', $field, $matches)) {
+				list($attribute, $name) = $matches;
+				$model = $get_class($name);
+				if (empty($model)) {
+					$entity->$name = null;
+					continue;
+				}
+				$return = $entity->$name = $model::first($entity->$attribute);
+			}
+		}
+		return $return;
+	}
+
+	/**
+	 * fetches the associated configuration
+	 *
+	 * @param object $entity current instance
+	 * @param string $field name of configuration to return
+	 * @return array client data
+	 */
+	public function value($entity, $field = null, array $options = array()) {
+		$entity->config = Configurations::first($entity->config_id);
+		return $entity->val($field, $options);
+	}
+
+}
+
+?>
---
+++ models/Configurations.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace radium\models;
+
+use lithium\util\Set;
+
+/**
+ * Works as a key-value store with db-backend
+ *
+ * Use like this:
+ *
+ * Configurations::write('title', 'Application'); // writes Application to title
+ * Configurations::read('title', 'unnamed App'); // 'unnamed App' will be default fallback
+ *
+ */
+class Configurations extends \radium\models\BaseModel {
+
+	/**
+	 * Stores the data schema.
+	 *
+	 * @see lithium\data\source\MongoDb::$_schema
+	 * @var array
+	 */
+	protected $_schema = array(
+		'_id' => array('type' => 'id'),
+		'name' => array('type' => 'string', 'default' => '', 'null' => false),
+		'slug' => array('type' => 'string', 'default' => '', 'null' => false),
+		'type' => array('type' => 'string', 'default' => 'string'),
+		'value' => array('type' => 'string'),
+		'data' => array('type' => 'array'),
+		'notes' => array('type' => 'string', 'default' => '', 'null' => false),
+		'status' => array('type' => 'string', 'default' => 'active', 'null' => false),
+		'created' => array('type' => 'datetime', 'default' => '', 'null' => false),
+		'updated' => array('type' => 'datetime'),
+		'deleted' => array('type' => 'datetime'),
+	);
+
+	/**
+	 * Validation rules
+	 *
+	 * @var array
+	 */
+	public $validates = array(
+		'_id' => array(
+			array('notEmpty', 'message' => 'a unique _id is required.', 'last' => true, 'on' => 'update'),
+		),
+		'type' => array(
+			array('notEmpty', 'message' => 'type is empty.'),
+			array('inList', 'list' => array('boolean', 'string', 'array'), 'message' => 'type must be valid.')
+		),
+		'name' => array(
+			array('notEmpty', 'message' => 'a name is required.'),
+		),
+		'slug' => array(
+			array('notEmpty', 'message' => 'a valid slug is required.', 'last' => true),
+			array('alphaNumeric', 'message' => 'only numbers and letters are allowed.', 'last' => true),
+		),
+		'status' => array(
+			array('notEmpty', 'message' => 'Status is empty.'),
+			array('inList', 'list' => array('active', 'inactive'), 'message' => 'Status must be a valid option.')
+		)
+	);
+
+	/**
+	 * Custom type options
+	 *
+	 * @var array
+	 */
+	public static $_types = array(
+		'boolean' => 'boolean',
+		'string' => 'string',
+		'array' => 'array',
+	);
+
+	/**
+	 * Sets default connection options and connects default finders.
+	 *
+	 * @see lithium\data\Model::__init()
+	 * @param array $options
+	 * @return void
+	 */
+	public static function __init(array $options = array()) {
+		parent::__init($options);
+
+		// auto-populate keys and vals
+		static::applyFilter('save', function ($self, $params, $chain) {
+			if ($params['entity']->type == 'array') {
+				$config = parse_ini_string($params['entity']->value);
+				$params['entity']->val = Set::expand($config);
+			}
+			return $chain->next($self, $params, $chain);
+		});
+	}
+
+	public static function dropdown() {
+		$configurations = static::find('array');
+		return Set::combine($configurations->data(), '/_id', '/name', '/status');
+	}
+
+	/**
+	 * return configured value
+	 *
+	 * Depending on type this can be a boolean, a string or an array.
+	 * For array, this can be used to retrieve sub-key of array, like this:
+	 *
+	 * val(array('field' => 'parentkey'))
+	 * val(array('field' => 'parentkey.subkey'))
+	 *
+	 * @see sy_core\model\Configs::get()
+	 * @param object $entity instance of current Record
+	 * @param array $options an array of options currently supported are
+	 *              - `default` : what to return, if nothing is found
+	 *              - `field`   : what field (in case of array) to return
+	 *              - `type`    : to force a certain type, ie. boolean
+	 * @return mixed whatever can be returned
+	 */
+	public function val($entity, $field = null, array $options = array()) {
+		$defaults = array('default' => null);
+		$options += $defaults;
+		switch ($entity->type) {
+			case 'boolean':
+				return (boolean) $entity->value;
+			case 'string':
+				return (string) $entity->value;
+			case 'array':
+				$config = $entity->val;
+				if (!empty($field)) {
+					if (array_key_exists($field, $config)) {
+						return $config[$field];
+					}
+				}
+				$field = '/'.str_replace('.', '/', $field).'/.';
+				$result = current(Set::extract($config, $field));
+				if (!empty($result)) {
+					return $result;
+				}
+				return (array) $config;
+		}
+		return $options['default'];
+	}
+
+	public function flat($entity) {
+		$config = Set::flatten($entity->val());
+		$result = array();
+		foreach ($config as $key => $value) {
+			$result[] = compact('key', 'value');
+		}
+		return $result;
+	}
+
+	/**
+	 * load a specific configuration, or retrieve a field from one.
+	 *
+	 * if just given a name, it returns the val() of that record. If you
+	 * pass in a default, you will return that, if nothing is found.
+	 * You can also use $options to request a certain field, see val()
+	 *
+	 * @see sy_core\model\Configs::val()
+	 * @param string $name name of confiuration to retrieve
+	 * @param string $default what to return, if nothing is found
+	 * @param array $options an array of options, currently all of
+	 *              Configs::val() are supported, see there.
+	 * @return mixed
+	 */
+	public static function get($name, $default = null, array $options = array()) {
+		$defaults = array('default' => $default, 'field' => null);
+		$options += $defaults;
+		$entity = static::first($name);
+		if (!$entity || !$entity->status) {
+			return $options['default'];
+		}
+		return $entity->val($options);
+	}
+}
+
+?>
