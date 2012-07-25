6cd43674f32871e8e07402274894808d2ca29cbf
d1rk <d1rk@gmx.de>
2012-07-25 21:30:54 +0200

    better mock data for configurations

--- tests/mocks/data/MockConfigurations.php
+++ tests/mocks/data/MockConfigurations.php
@@ -30,24 +30,24 @@ class MockConfigurations extends \radium\models\Configurations {
 				return new DocumentSet(array('data' => array(
 					array(
 						'_id' => 1,
-						'name' => 'foo',
-						'slug' => 'foo',
+						'name' => 'first',
+						'slug' => 'first',
 						'status' => 'active',
 						'created' => $now,
 						'modified' => $now
 					),
 					array(
 						'_id' => 2,
-						'name' => 'foo',
-						'slug' => 'foo',
-						'status' => 'active',
+						'name' => 'second',
+						'slug' => 'second',
+						'status' => 'inactive',
 						'created' => $now,
 						'modified' => $now
 					),
 					array(
 						'_id' => 3,
-						'name' => 'foo',
-						'slug' => 'foo',
+						'name' => 'third',
+						'slug' => 'third',
 						'status' => 'active',
 						'created' => $now,
 						'modified' => $now
