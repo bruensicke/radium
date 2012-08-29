2cb50ef7bab3f85c1650ff6ff9aeab8ab2749710
d1rk <d1rk@gmx.de>
2012-08-29 10:41:27 +0200

    add notes to configurations overview

--- views/mustache/configurations/table.html.php
+++ views/mustache/configurations/table.html.php
@@ -2,8 +2,8 @@
 	<colgroup>
 		<col width="70" />
 		<col width="100" />
+		<col width="180" />
 		<col width="*" />
-		<col width="120" />
 		<col width="120" />
 		<col width="180" />
 	</colgroup>
@@ -12,7 +12,7 @@
 			<th>Status</th>
 			<th>Type</th>
 			<th>Name</th>
-			<th>Description</th>
+			<th>Notes</th>
 			<th>Created</th>
 			<th>Actions</th>
 		</tr>
@@ -24,16 +24,16 @@
 		</tr>
 	{{/configurations}}
 	{{#configurations}}
-		<tr{{#deleted}} class="low"{{/deleted}}>
+		<tr{{#deleted}} class="muted"{{/deleted}}>
 			<td><span class="label label_{{ status }}">{{ status }}</span></td>
 			<td><span class="label label_{{ type }}">{{ type }}</span></td>
-			<td><a href="<?=$this->url('/'); ?>radium/configurations/view/{{ _id }}"><small class="low">{{ name }}</small></a></td>
-			<td>{{ description }}</td>
+			<td><a href="<?=$this->url('/'); ?>radium/configurations/view/{{ _id }}">{{ name }}</a></td>
+			<td class="muted">{{ notes }}</td>
 			<td data-datetime="{{ created }}"></td>
 			<td>
 				<div class="btn-group">
-					<a class="btn btn" href="<?=$this->url('/'); ?>radium/configurations/edit/{{ _id }}"><i class="icon-pencil"></i> Edit</a>
-					<a class="btn btn dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
+					<a class="btn btn-small" href="<?=$this->url('/'); ?>radium/configurations/edit/{{ _id }}"><i class="icon-pencil"></i> Edit</a>
+					<a class="btn btn-small dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
 					<ul class="dropdown-menu">
 						<li><a href="<?=$this->url('/'); ?>radium/configurations/view/{{ _id }}"><i class="icon-info-sign"></i> View</a></li>
 						<li><a href="<?=$this->url('/'); ?>radium/configurations/edit/{{ _id }}"><i class="icon-pencil"></i> Edit</a></li>
