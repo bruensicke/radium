b2e8821c469f3a0bd6f80912ff52eea7ea3b0e8e
d1rk <d1rk@gmx.de>
2012-09-04 12:26:49 +0200

    add BaseController::redirect() and _url()

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -33,6 +33,48 @@ class BaseController extends \lithium\action\Controller {
 	}
 
 	/**
+	 * automatic supplement of library for redirects
+	 *
+	 * @see lithium\net\http\Router::match()
+	 * @see lithium\action\Controller::$response
+	 * @see lithium\action\Controller::redirect()
+	 * @param mixed $url The location to redirect to, provided as a string relative to the root of
+	 *              the application, a fully-qualified URL, or an array of routing parameters to be
+	 *              resolved to a URL. Post-processed by `Router::match()`.
+	 * @param array $options Options when performing the redirect. Available options include:
+	 *              - `'status'` _integer_: The HTTP status code associated with the redirect.
+	 *                Defaults to `302`.
+	 *              - `'head'` _boolean_: Determines whether only headers are returned with the
+	 *                response. Defaults to `true`, in which case only headers and no body are
+	 *                returned. Set to `false` to render a body as well.
+	 *              - `'exit'` _boolean_: Exit immediately after rendering. Defaults to `false`.
+	 *                Because `redirect()` does not exit by default, you should always prefix calls
+	 *                with a `return` statement, so that the action is always immediately exited.
+	 * @return object Returns the instance of the `Response` object associated with this controller.
+	 */
+	public function redirect($url, array $options = array()) {
+		return parent::redirect($this->_url($url), $options);
+	}
+
+	/**
+	 * automatic supplement of library for redirects
+	 *
+	 * @param mixed $url The location to redirect to, provided as a string relative to the root of
+	 *              the application, a fully-qualified URL, or an array of routing parameters to be
+	 *              resolved to a URL. Post-processed by `Router::match()`.
+	 * @param mixed $url The location including the library parameter
+	 */
+	protected function _url($url) {
+		if (is_string($url)) {
+			$url = array($url);
+		}
+		if (!empty($this->library) && empty($url['library'])) {
+			$url['library'] = $this->library;
+		}
+		return $url;
+	}
+
+	/**
 	 * Generates options out of named params
 	 *
 	 * @param string $defaults all default options you want to have set
