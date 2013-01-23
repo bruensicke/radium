17bb3f141626f693ca4814e87f7e8d9074372a04
d1rk <d1rk@gmx.de>
2013-01-23 09:45:23 +0100

    added MockForeignModel

---
+++ tests/mocks/MockForeignModel.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace radium\tests\mocks;
+
+use lithium\data\collection\DocumentSet;
+use lithium\data\entity\Document;
+
+class MockForeignModel extends \radium\models\BaseModel {
+
+	protected $_meta = array(
+		'connection' => false,
+	);
+
+	public static function find($type = 'all', array $options = array()) {
+		$now = date('Y-m-d h:i:s');
+		switch ($type) {
+			case 'first':
+				return new Document(array('data' => array(
+					'_id' => 1,
+					'name' => 'foo',
+					'slug' => 'foo',
+					'status' => 'active',
+					'created' => $now,
+					'modified' => $now
+				)));
+				break;
+			case 'all':
+			default :
+				return new DocumentSet(array('data' => array(
+					array(
+						'_id' => 1,
+						'name' => 'first',
+						'slug' => 'first',
+						'status' => 'active',
+						'created' => $now,
+						'modified' => $now
+					),
+					array(
+						'_id' => 2,
+						'name' => 'second',
+						'slug' => 'second',
+						'status' => 'inactive',
+						'created' => $now,
+						'modified' => $now
+					),
+					array(
+						'_id' => 3,
+						'name' => 'third',
+						'slug' => 'third',
+						'status' => 'active',
+						'created' => $now,
+						'modified' => $now
+					)
+				)));
+				break;
+		}
+	}
+}
+
+?>
