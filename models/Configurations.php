d95e3d65b290f6608525e4b0428bebff44ebc38c
Dirk Brünsicke <d1rk@gmx.de>
2013-04-12 11:03:49 +0300

    removed __init method from Configurations

--- models/Configurations.php
+++ models/Configurations.php
@@ -76,17 +76,6 @@ class Configurations extends \radium\models\BaseModel {
 	);
 
 	/**
-	 * Sets default connection options and connects default finders.
-	 *
-	 * @see lithium\data\Model::__init()
-	 * @param array $options
-	 * @return void
-	 */
-	public static function __init(array $options = array()) {
-		parent::__init($options);
-	}
-
-	/**
 	 * return configured value
 	 *
 	 * Depending on type this can be a boolean, a string or an array.
