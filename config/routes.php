e50a2cbb4acee29dc1f4e2332c0a7c16ec29c07b
d1rk <d1rk@gmx.de>
2013-05-29 13:34:51 +0200

    add comment about recommended routes

--- config/routes.php
+++ config/routes.php
@@ -9,6 +9,9 @@ Router::connect('/radium/{:controller}/{:action}', array('library' => 'radium'))
 Router::connect('/radium/{:controller}', array('library' => 'radium'));
 Router::connect('/radium', array('library' => 'radium', 'controller' => 'pages', 'action' => 'file'));
 
+/*
+ we encourage you to add routes to your app-routes file, that look like this:
+ */
 // Router::connect('/{:controller}/{:action}/{:id:[0-9a-f]{24}}.{:type}', array('id' => null));
 // Router::connect('/{:controller}/{:action}/{:id:[0-9a-f]{24}}');
 
