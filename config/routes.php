707a0a6a99a254dbba418ba68f82c3e3a03d7024
d1rk <d1rk@gmx.de>
2013-05-29 13:34:41 +0200

    add /api route

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,7 @@
 <?php
 use lithium\net\http\Router;
 
+Router::connect('/radium/api/{:args}', array('type' => 'json', 'library' => 'radium'), array('continue' => true));
 Router::connect('/radium/{:controller}/{:action}/{:id:[0-9a-f]{24}}/{:args}', array('library' => 'radium'));
 Router::connect('/radium/{:controller}/{:action}/{:id:[0-9a-f]{24}}', array('library' => 'radium'));
 Router::connect('/radium/{:controller}/{:action}/{:args}', array('library' => 'radium'));
