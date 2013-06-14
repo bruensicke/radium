1e2bf063eed1d41420f6b5fd093c38cf9f22a137
d1rk <d1rk@gmx.de>
2013-06-14 09:17:55 +0200

    allow app to overwrite scaffold-templates

--- controllers/ScaffoldController.php
+++ controllers/ScaffoldController.php
@@ -21,6 +21,7 @@ class ScaffoldController extends \radium\controllers\BaseController {
 		$this->controller = $this->request->controller;
 		$this->library = $this->request->library;
 
+		$this->_render['paths']['template'][] = '{:library}/views/scaffold/{:template}.{:type}.php';
 		$this->_render['paths']['template'][] = RADIUM_PATH . '/views/scaffold/{:template}.{:type}.php';
 		$this->_scaffold();
 		if ($this->request->is('ajax')) {
