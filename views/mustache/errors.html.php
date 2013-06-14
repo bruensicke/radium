6646d6025daed7632f03cfbf9e2d3dd791f79376
d1rk <d1rk@gmx.de>
2013-06-14 09:18:22 +0200

    improve display of errors in mustache template

--- views/mustache/errors.html.php
+++ views/mustache/errors.html.php
@@ -1,14 +1,12 @@
-{{#errors}}
 <div class="alert alert-block alert-error">
 	<h4>Errors!</h4>
 	<p>Some errors occured, you should double-check your inputs</p>
 	<dl>
-		{{#.}}
+		{{#errors}}
 			<dt>{{ key }}</dt>
 			{{#value}}
 				<dd>{{ . }}</dd>
 			{{/value}}
-		{{/.}}
+		{{/errors}}
 	</dl>
 </div>
-{{/errors}}
