fee855ab5b6213bbc85c30e53e867306cd23bb09
d1rk <d1rk@gmx.de>
2013-06-19 20:50:26 +0200

    fix indentation of contents view

--- views/contents/view.html.php
+++ views/contents/view.html.php
@@ -1,18 +1,18 @@
 <ul class="breadcrumb">
-	<li><?=$this->html->link('Home', '/');?> <span class="divider">/</span></li>
-	<li><?=$this->html->link('radium', '/radium');?> <span class="divider">/</span></li>
-	<li><?=$this->html->link('Contents', array('library' => 'radium', 'action' => 'index'));?> <span class="divider">/</span></li>
+	<li><?= $this->html->link('Home', '/');?> <span class="divider">/</span></li>
+	<li><?= $this->html->link('radium', '/radium');?> <span class="divider">/</span></li>
+	<li><?= $this->html->link('Contents', array('library' => 'radium', 'action' => 'index'));?> <span class="divider">/</span></li>
 	<li class="active">
-		<?=$this->title(sprintf('Contents: %s', $content->name)); ?>
+		<?= $this->title(sprintf('Contents: %s', $content->name)); ?>
 		<span class="label label_<?= $content->status ?>"><?= $content->status ?></span>
 	</li>
 	<li class="pull-right">
-		<?=$this->html->link('edit', array('library' => 'radium', 'action' => 'edit', 'args' => array('id' => (string) $content->_id)));?>
+		<?= $this->html->link('edit', array('library' => 'radium', 'action' => 'edit', 'args' => array('id' => (string) $content->_id)));?>
 	</li>
 </ul>
 
 <div class="page-header">
-	<h1><?=$this->title(); ?> <small>See content details.</small></h1>
+	<h1><?= $this->title(); ?> <small>See content details.</small></h1>
 </div>
 <p><?= $content->notes ?></p>
 
