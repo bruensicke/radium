60328a4d9d323901e31998bae7a59e6d9b8629fc
d1rk <d1rk@gmx.de>
2013-06-20 17:51:37 +0200

    configure Converter adapters

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,5 +1,23 @@
 <?php
+/**
+ * radium: lithium application framework
+ *
+ * @copyright     Copyright 2013, brünsicke.com GmbH (http://bruensicke.com)
+ * @license       http://opensource.org/licenses/BSD-3-Clause The BSD License
+ */
+
+use radium\data\Converter;
 
 define('RADIUM_PATH', dirname(__DIR__));
+
+Converter::config(array(
+	'plain' => array(
+		'adapter' => 'Plain',
+	),
+	'html' => array(
+		'adapter' => 'Html',
+	),
+));
+
 
 ?>
