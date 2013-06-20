566639383bea4b88fdbf9d359c2587f98691cfac
d1rk <d1rk@gmx.de>
2013-06-20 17:52:11 +0200

    stubbing Markdown and Mustache converter adapter

---
+++ extensions/adapter/converters/Markdown.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * radium: lithium application framework
+ *
+ * @copyright     Copyright 2013, brünsicke.com GmbH (http://bruensicke.com)
+ * @license       http://opensource.org/licenses/BSD-3-Clause The BSD License
+ */
+
+namespace radium\extensions\adapter\converters;
+
+class Markdown extends \lithium\core\Object {
+
+	/**
+	 * returns rendered content
+	 *
+	 * @param string $content input content
+	 * @param array $data additional data to be passed into render context
+	 * @param array $options an array with additional options
+	 * @return string content as given
+	 * @filter
+	 */
+	public function get($content, $data = array(), array $options = array()) {
+		$defaults = array('allowed' => true);
+		$options += $defaults;
+		$params = compact('content', 'data', 'options');
+		return $this->_filter(__METHOD__, $params, function($self, $params) {
+			// TODO: parse with markdown parser
+			return $params['content'];
+		});
+	}
+
+}
+?>
