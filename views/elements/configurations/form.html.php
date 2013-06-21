c5213f14266a421ce5cd8f5cdb3ad0fc56b30a48
d1rk <d1rk@gmx.de>
2013-06-21 12:19:48 +0200

    updated Configurations form
    - added checkbox for boolean values
    - added visible checkbox
    - added input for category

--- views/elements/configurations/form.html.php
+++ views/elements/configurations/form.html.php
@@ -3,16 +3,28 @@
 	<div class="span4">
 		<legend><?= $this->scaffold->human ?> meta</legend>
 		<div class="well">
+			<p>Visibility in settings page</p>
+			<div class="input-prepend">
+				<span class="add-on"><?= $this->form->checkbox('visible');?></span>
+				<?= $this->form->text('category', array('class' => 'span2'));?>
+			</div>
 			<?= $this->scaffold->render('form.meta'); ?>
 		</div>
 	</div>
 
 	<div class="span8">
 		<legend><?= $this->scaffold->human ?> details</legend>
-		<div class="well">
+		<div class="well type_boolean">
+			<p class="muted">Do you want to enable this setting?</p>
+			<div class="input-prepend input-append">
+				<span class="add-on">Enabled</span>
+				<span class="add-on"><?= $this->form->checkbox('value');?></span>
+			</div>
+		</div>
+		<div class="well type_string type_list type_array type_ini type_neon">
 			<?= $this->form->field('value', array(
 				'type' => 'textarea',
-				'class' => 'input-block-level',
+				'class' => 'input-block-level autogrow',
 				'style' => 'height: 370px;',
 			));?>
 		</div>
