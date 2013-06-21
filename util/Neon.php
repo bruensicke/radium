7146b20a462e7c7e82130c51f9e89d49a07e6669
d1rk <d1rk@gmx.de>
2013-06-21 10:39:52 +0200

    Add Neon Proxy at util for easier usage

---
+++ util/Neon.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * radium: lithium application framework
+ *
+ * @copyright     Copyright 2013, brünsicke.com GmbH (http://bruensicke.com)
+ * @license       http://opensource.org/licenses/BSD-3-Clause The BSD License
+ */
+
+namespace radium\util;
+
+use lithium\core\Libraries;
+use Neon\Neon as NeonRenderer;
+
+class Neon {
+
+	public static $_renderer = null;
+
+	public static function encode($input, $options = null) {
+		return static::renderer()->encode($input, $options);
+	}
+
+	public static function decode($content) {
+		return static::renderer()->decode($content);
+	}
+
+	public static function renderer() {
+		if (is_null(static::$_renderer)) {
+			Libraries::add('Neon', array('path' => RADIUM_PATH . '/libraries/neon'));
+			static::$_renderer = new NeonRenderer();
+		}
+		return static::$_renderer;
+	}
+
+}
+
+?>
