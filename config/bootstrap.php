dcac3b6e1af89090c0326c935be71c554d62efe9
d1rk <d1rk@gmx.de>
2013-06-25 22:29:57 +0200

    remove routes for static assets, use media loader instead

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,9 +6,33 @@
  * @license       http://opensource.org/licenses/BSD-3-Clause The BSD License
  */
 
+define('RADIUM_PATH', dirname(__DIR__));
+
+use lithium\action\Dispatcher;
+use lithium\action\Response;
+use lithium\net\http\Media;
+
 use radium\data\Converter;
 
-define('RADIUM_PATH', dirname(__DIR__));
+/*
+ * this filter allows automatic linking and loading of assets from `webroot` folder
+ */
+Dispatcher::applyFilter('_callable', function($self, $params, $chain) {
+	list($tmp, $library, $asset) = explode('/', $params['request']->url, 3) + array("", "");
+	if ($asset && $library == 'radium' && ($path = Media::webroot($library)) && file_exists($file = "{$path}/{$asset}")) {
+		return function() use ($file) {
+			$info = pathinfo($file);
+			$media = Media::type($info['extension']);
+			$content = (array) $media['content'];
+
+			return new Response(array(
+				'headers' => array('Content-type' => reset($content)),
+				'body' => file_get_contents($file)
+			));
+		};
+	}
+	return $chain->next($self, $params, $chain);
+});
 
 Converter::config(array(
 	'array' => array(
