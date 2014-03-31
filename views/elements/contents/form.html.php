54d584d26b35b58b0255632927cb4345f742efeb
Maik Ploigt <maik@ploigt.de>
2014-03-31 14:53:33 +0200

    Added well class to scaffold form.

--- views/elements/contents/form.html.php
+++ views/elements/contents/form.html.php
@@ -2,19 +2,23 @@
 
 	<div class="col-md-4">
 		<h3><?= $this->scaffold->human ?> meta</h3>
-		<div class="form-group">
-			<?= $this->scaffold->render('form.meta'); ?>
+		<div class="well">
+			<div class="form-group">
+				<?= $this->scaffold->render('form.meta'); ?>
+			</div>
 		</div>
 	</div>
 
 	<div class="col-md-8">
 		<h3><?= $this->scaffold->human ?> details</h3>
-		<div class="form-group">
-			<?= $this->form->field('body', array(
-				'type' => 'textarea',
-				'class' => 'form-control',
-				'style' => 'height: 370px;',
-			));?>
+		<div class="well">
+			<div class="form-group">
+				<?= $this->form->field('body', array(
+					'type' => 'textarea',
+					'class' => 'form-control',
+					'style' => 'height: 370px;',
+				));?>
+			</div>
 		</div>
 	</div>
 
--- views/elements/scaffold/form.html.php
+++ views/elements/scaffold/form.html.php
@@ -14,16 +14,20 @@ $readonly = isset($readonly)
 
 	<div class="col-md-4">
 		<h3><?= $this->scaffold->human ?> meta</h3>
-		<div class="form-group">
-			<?= $this->scaffold->render('form.meta', compact('skip', 'readonly')); ?>
+		<div class="well">
+			<div class="form-group">
+				<?= $this->scaffold->render('form.meta', compact('skip', 'readonly')); ?>
+			</div>
 		</div>
 	</div>
 
 	<div class="col-md-8">
 		<h3><?= $this->scaffold->human ?> details</h3>
-		<div class="form-group">
-			<?php $fields = $schema->names(); $skip += $meta; ?>
-			<?= $this->scaffold->render('form.fields', compact('fields', 'skip', 'readonly')); ?>
+		<div class="well">
+			<div class="form-group">
+				<?php $fields = $schema->names(); $skip += $meta; ?>
+				<?= $this->scaffold->render('form.fields', compact('fields', 'skip', 'readonly')); ?>
+			</div>
 		</div>
 	</div>
 
