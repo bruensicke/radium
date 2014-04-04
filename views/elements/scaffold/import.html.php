5ba5b78dca29d0423283ffdaefaa7fa71114aa62
d1rk <d1rk@gmx.de>
2014-04-04 03:16:53 +0200

    fix file-upload on scaffold import

--- views/elements/scaffold/import.html.php
+++ views/elements/scaffold/import.html.php
@@ -1,29 +1,9 @@
-<div class="row">
+<div id="uploader"></div>
+<div id="uploadResult"></div>
 
-	<div class="span8">
-		<div id="uploader"></div>
-		<div id="uploadResult"></div>
-	</div>
-
-	<div class="span4">
-		<div class="well">
-			<h3>Data import</h3>
-			<p>Select and upload files that you exported.</p>
-		</div>
-	</div>
-
-
-</div>
-<?=$this->html->style('/radium/css/import'); ?>
 <script type="text/javascript" charset="utf-8">
-head.js(
-	{ fineuploader: "<?php echo $this->path('/li3_bootstrap/js/fineuploader.min.js'); ?>"},
-	{ uploader: "<?php echo $this->path('/radium/js/import.js'); ?>"}
-);
-head.ready(function() {
-	uploader.api = "<?php echo $this->url($this->scaffold->action('import')); ?>";
-	uploader.template = '<?php echo implode(explode("\n", $this->_render('element', 'import'))); ?>';
-	uploader.button = '<div><i class="icon-upload-alt icon-white"></i> import files</div>';
-	uploader.init();
-});
+uploader.api = "<?php echo $this->url($this->scaffold->action('import')); ?>";
+uploader.template = '<?php echo implode(explode("\n", $this->_render('element', 'import'))); ?>';
+uploader.button = '<div><i class="fa fa-2x fa-cloud-upload fa-fw"></i> import files</div>';
+uploader.init();
 </script>
