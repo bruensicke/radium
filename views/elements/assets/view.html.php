0f8e083f55fc1ab527a9043acaf6428cbc070bc3
d1rk <d1rk@gmx.de>
2014-04-04 03:16:39 +0200

    showing content of import-files on assets

--- views/elements/assets/view.html.php
+++ views/elements/assets/view.html.php
@@ -4,6 +4,9 @@ switch($asset->type) {
 	case 'plain':
 		echo sprintf('<div class="plaintext"><pre>%s</pre></div>', $asset->file->getBytes());
 	break;
+	case 'import':
+		echo $this->scaffold->render('data', array('data' => \lithium\util\Set::flatten($asset->decode())));
+	break;
 	case 'image':
 	default:
 		#echo $asset->body();
