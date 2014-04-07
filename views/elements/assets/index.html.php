f8a6df6a019e53af15536f060476f96b5db43d55
Maik Ploigt <maik@ploigt.de>
2014-04-07 10:49:27 +0200

    changed wrong icon in drop down

--- views/elements/assets/index.html.php
+++ views/elements/assets/index.html.php
@@ -6,6 +6,6 @@
 <script type="text/javascript" charset="utf-8">
 uploader.api = "<?php echo $this->url($this->scaffold->action('upload')); ?>";
 uploader.template = '<?php echo implode(explode("\n", $this->_render('element', 'import'))); ?>';
-uploader.button = '<div><i class="fa fa-2x fa-upload fa-fw"></i> UPLOAD</div>';
+uploader.button = '<div><i class="fa fa-2x fa-download fa-fw"></i> UPLOAD</div>';
 uploader.init();
 </script>
