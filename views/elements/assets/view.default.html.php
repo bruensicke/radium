393da91e4e7d4c85d21348b2dd445cf14c5cde50
Maik Ploigt <maik@ploigt.de>
2014-04-07 16:05:25 +0200

    added audio and video view templates for assets.

--- views/elements/assets/view.default.html.php
+++ views/elements/assets/view.default.html.php
@@ -1,5 +1,5 @@
 <hr />
 <?php
 unset($this->scaffold->object['file']); // no need to show data from grid.fs
-echo $this->scaffold->render('data', array('data' => \lithium\util\Set::flatten($this->scaffold->object->data()))); ?>
+echo $this->scaffold->render('data', array('data' => \lithium\util\Set::flatten($this->scaffold->object->data())));
 ?>
--- views/elements/assets/view.html.php
+++ views/elements/assets/view.html.php
@@ -1,11 +1,9 @@
 <?php
 $asset = $this->scaffold->object;
-if (in_array($asset->type, array('import', 'plain', 'image'))) {
+if (in_array($asset->type, array('import', 'plain', 'image', 'audio', 'video'))) {
 	echo $this->_render('element', sprintf('assets/view.%s', $asset->type));
 } else {
 	echo $this->_render('element', 'assets/view.default');
 }
 unset($this->scaffold->object['file']); // no need to show data from grid.fs
 ?>
-<hr />
-<?= $this->scaffold->render('data', array('data' => \lithium\util\Set::flatten($this->scaffold->object->data()))); ?>
