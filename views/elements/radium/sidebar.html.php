8f2ba278c162062c9278ca955b7d0331eb9fe2ca
d1rk <d1rk@gmx.de>
2014-07-16 14:26:31 +0200

    use file-based navigation in sidebar

--- views/elements/radium/sidebar.html.php
+++ views/elements/radium/sidebar.html.php
@@ -1,12 +1,3 @@
 <aside class="sidebar">
-<?php
-$nav = $this->Navigation->group('sidebar');
-if (empty($nav)) {
-	$nav = $this->Navigation->render(array(
-		array('name' => 'Contents', 'icon' => 'file-text'),
-		array('name' => 'Configurations', 'icon' => 'cogs'),
-	));
-}
-echo $nav;
-?>
+	<?= $this->Navigation->group('sidebar'); ?>
 </aside>
